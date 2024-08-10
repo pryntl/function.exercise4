c8010f878eeaf3961db11b1f031c934a39fd2ecd
pryntl <pryntl2007@gmail.com>
2024-08-10 14:18:41 +0330

    feat [java script]
    added a feature which collects two arrays and makes one.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function collect(array1, array2) {
+  let output = 0;
+  if (typeof array1 == "object" && typeof array2 == "object") {
+    output = array1.concat(array2);
+  } else if (typeof array1 == "string" || typeof array2 == "string") {
+    output = array1.split().concat(array2.split());
+  }
+  return output;
+}
+console.log(collect([1, 2], [3, 4]));
+console.log(collect("1", "2"));
